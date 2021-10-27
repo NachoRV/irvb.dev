2d436159fb9265662935c8610c6ee61bcb00a758
irvb <irvb@MacBook-Pro-de-irvb.local>
2021-10-27 20:59:10 +0200

    SEO tw

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,8 +1,8 @@
 <template>
-<div>
-  <article>
-    <!-- <AppSearchInput /> -->
-    <!-- <nav>
+  <div>
+    <article>
+      <!-- <AppSearchInput /> -->
+      <!-- <nav>
       <ul>
         <li v-for="link of article.toc" :key="link.id">
           <NuxtLink
@@ -11,20 +11,20 @@
         </li>
       </ul>
     </nav> -->
-    <img :src="article.img" :alt="article.alt" />
-    <div class="article">
-      <h1>{{ article.title }}</h1>
-      <p>{{ article.description }}</p>
-      <p>Article last updated: {{ formatDate(article.updatedAt) }}</p>
-      <nuxt-content :document="article" />
-    </div>
-    <!-- <author :author="article.author" /> -->
-    <prev-next :prev="prev" :next="next" />
-  </article>
-</div>
+      <img :src="article.img" :alt="article.alt" />
+      <div class="article">
+        <h1>{{ article.title }}</h1>
+        <p>{{ article.description }}</p>
+        <p>Article last updated: {{ formatDate(article.updatedAt) }}</p>
+        <nuxt-content :document="article" />
+      </div>
+      <!-- <author :author="article.author" /> -->
+      <prev-next :prev="prev" :next="next" />
+    </article>
+  </div>
 </template>
 <script>
-import getSiteMeta from  '../../utils/getSiteMeta';
+import getSiteMeta from '../../utils/getSiteMeta'
 export default {
   async asyncData({ $content, params }) {
     const article = await $content('articles', params.slug).fetch()
@@ -41,58 +41,73 @@ export default {
       next,
     }
   },
-head() {
-  return {
-    title: this.article.title,
-    meta: [
-      ...this.meta,
-      {
-        property: "article:published_time",
-        content: this.article.createdAt,
-      },
-      {
-        property: "article:modified_time",
-        content: this.article.updatedAt,
-      },
-      {
-        property: "article:tag",
-        content: this.article.tags ? this.article.tags.toString() : "",
-      },
-      { name: "twitter:label1", content: "irvb" },
-      { name: "twitter:data1", content: "Bob Ross" },
-      { name: "twitter:label2", content: "Filed under" },
-      {
-        name: "twitter:data2",
-        content: this.article.tags ? this.article.tags.toString() : "",
-      },
-    ],
-    link: [
-      {
-        hid: "canonical",
-        rel: "canonical",
-        href: `https://irvb-dev-nachorv.vercel.app/blog/${this.$route.params.slug}`,
-      },
-    ],
-  };
-},
-  computed: {
-  meta() {
-    const metaData = {
-      type: "article",
+  head() {
+    return {
       title: this.article.title,
-      description: this.article.description,
-      url: `${this.$config.baseUrl}/articles/${this.$route.params.slug}`,
-      mainImage: this.article.image,
-    };
-    return getSiteMeta(metaData);
-  }
-},
+      meta: [
+        ...this.meta,
+        {
+          property: 'article:published_time',
+          content: this.article.createdAt,
+        },
+        {
+          property: 'article:modified_time',
+          content: this.article.updatedAt,
+        },
+        {
+          property: 'article:tag',
+          content: this.article.tags ? this.article.tags.toString() : '',
+        },
+        { name: 'twitter:site', content: '@I_R_V_B' },
+        { name: 'twitter:card', content: 'summary_large_image' },
+        {
+          hid: 'twitter:url',
+          name: 'twitter:url',
+          content: this.$config.baseUrl,
+        },
+        {
+          hid: 'twitter:title',
+          name: 'twitter:title',
+          content: this.article.title,
+        },
+        {
+          hid: 'twitter:description',
+          name: 'twitter:description',
+          content:this.article.description,
+        },
+        {
+          hid: 'twitter:image',
+          name: 'twitter:image',
+          content: this.article.image,
+        },
+      ],
+      link: [
+        {
+          hid: 'canonical',
+          rel: 'canonical',
+          href: `${this.$config.baseUrl}/blog/${this.$route.params.slug}`,
+        },
+      ],
+    }
+  },
+  computed: {
+    meta() {
+      const metaData = {
+        type: 'article',
+        title: this.article.title,
+        description: this.article.description,
+        url: `${this.$config.baseUrl}/articles/${this.$route.params.slug}`,
+        mainImage: this.article.image,
+      }
+      return getSiteMeta(metaData)
+    },
+  },
   methods: {
     formatDate(date) {
       const options = { year: 'numeric', month: 'long', day: 'numeric' }
       return new Date(date).toLocaleDateString('en', options)
     },
-  }
+  },
 }
 </script>
 <style>
@@ -100,10 +115,10 @@ img {
   width: 100%;
 }
 .article {
- margin: 1rem;
- margin-bottom: 4rem;
+  margin: 1rem;
+  margin-bottom: 4rem;
 }
-@media (min-width: 640px) { 
+@media (min-width: 640px) {
   .article {
     margin: 0rem;
     margin-bottom: 4rem;
